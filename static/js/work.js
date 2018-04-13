67fcc195ee0f28c79d161fdc8f81f0e5edf1ae72
GarnixJu <ambitious_816@hotmail.com>
2018-04-13 14:02:39 +0800

    change css style

--- static/js/work.js
+++ static/js/work.js
@@ -60,7 +60,8 @@ var csrftoken = getCookie('csrftoken');
 				replace(/(\r\n|\n)/g, "<br />");
 				// console.log(content);
 				// $('#correct-sec').html(content);
-				document.getElementById("show-text").innerHTML = content;
+				document.getElementById("show-text").innerHTML =  "<br />"  + content;
+				
                 // document.getElementById("show-text").textContent = content.result;
             }, 
             error: function(XMLHttpRequest, textStatus, errorThrown) { 
