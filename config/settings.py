3b7121ab54b0c73015ad67765a40d42cf128e187
ya2015 <ambitious_816@hotmail.com>
2018-04-22 21:23:40 +0800

    set allow host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nfe9$j^eg9y-^gj@oa#g&%na3svaki2!*ng4on-0tck#415oq$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
