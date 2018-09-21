ab2b861fd571306061cb5992cdd9e76719c74527
JJC <jjc@nlplab.cc>
2018-09-21 21:49:58 +0800

    modify gec view

--- gec/views.py
+++ gec/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import requests
 
-API_URL = 'http://nlp-thor.cs.nthu.edu.tw:1214/translate/'
+API_URL = 'http://thor.nlplab.cc:1214/translate/'
 HEADERS = {'Content-Type': 'application/json'}
 
 
+# @csrf_exempt
 def correct_it(request):
     if request.is_ajax() and request.method == 'POST':
         r = requests.post(API_URL, headers=HEADERS, data=request.body)
