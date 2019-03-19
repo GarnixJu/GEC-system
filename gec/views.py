65947c0f865e63dc1edeca4c8a7f03d60007b440
JJC <jjc@nlplab.cc>
2019-03-19 18:39:02 +0800

    add GET api

--- gec/views.py
+++ gec/views.py
@@ -14,13 +14,17 @@ def correct_it(request):
     if request.is_ajax() and request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         text = data.get('text', '')
+    elif request.method == 'GET':
+        text = request.GET.get('text', '')
+        data = {'text': text}
+    else:
+        data = {}
 
-        # tokenize text using spaCy
-        tokenized_text = '\n'.join('\n'.join(' '.join(token.text for token in sent) for sent in nlp(line.strip()).sents) for line in text.splitlines() if line.strip())
-        # correct it
-        corrected_text = translate(tokenized_text)
-        data['result'] = corrected_text
-        diff = [' '.join(wdiff(before, after)) for before, after in zip(tokenized_text.splitlines(), corrected_text.splitlines())]
-        data['word_diff'] = '\n'.join(diff)
-        data['word_diff_by_sent'] = diff
-        return JsonResponse(data)
+    tokenized_text = '\n'.join('\n'.join(' '.join(token.text for token in sent) for sent in nlp(line.strip()).sents) for line in text.splitlines() if line.strip())
+    corrected_text = translate(tokenized_text)
+    data['result'] = corrected_text
+    diff = [' '.join(wdiff(before, after)) for before, after in zip(tokenized_text.splitlines(), corrected_text.splitlines())]
+    data['word_diff'] = '\n'.join(diff)
+    data['word_diff_by_sent'] = diff
+    return JsonResponse(data)
+
